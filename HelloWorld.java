7bb2e8b12e187047ce096b14c5d1a94db64a78cf
Sangeetha712 <73005258+Sangeetha712@users.noreply.github.com>
2020-10-16 22:13:07 -0400

    Add files via upload
    First Java program

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		System.out.println("2: " + i);
+		
+		for(;i<12; i++){
+		
+			System.out.println(i);
+			
+			i=10;
+			
+	    }
+		System.out.println("1: " + i);
+		
+    }
+}
